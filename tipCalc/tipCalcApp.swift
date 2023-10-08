744e9c52fce15c62eefca1f85d160ad9a253ed3c
George  Ikwegbu Chinedu <39649680+gikwegbu@users.noreply.github.com>
2023-10-08 11:14:04 +0100

    Updated the @main value for the displaying screen

--- tipCalc/tipCalcApp.swift
+++ tipCalc/tipCalcApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct tipCalcApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+					calcScreen()
         }
     }
 }
